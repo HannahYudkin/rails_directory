a7e17a72da5fb8c0f3edf6121af9a7040b6e91a4
HannahYudkin <hannahyudkin@gmail.com>
2020-09-22 15:31:09 -0400

    Add footer.

---
+++ app/javascript/packs/components/footerbottom.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card height="150">
+    <v-footer
+      absolute
+      class="font-weight-medium"
+      :src="require('../assets/HeartBackground.jpg')"
+    >
+      <v-col class="text-center" cols="12" align="center" justify="center">
+        {{ new Intl.DateTimeFormat("en-US", { month: "long" }).format(date) }}
+        {{ new Date().getDate() }}, {{ new Date().getFullYear() }} -
+        <strong>Hannah Yudkin </strong>
+      </v-col>
+    </v-footer>
+  </v-card>
+</template>
+
+<style scoped>
+.font-weight-medium {
+  background-image: url("../assets/HeartBackground.jpg");
+  background-size: 100%;
+}
+</style>
