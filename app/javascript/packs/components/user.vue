2be2cb2689539c0c071b4780a82dd5e6606c110e
HannahYudkin <hannahyudkin@gmail.com>
2020-09-22 16:44:28 -0400

    Fixed errors.

--- app/javascript/packs/components/user.vue
+++ app/javascript/packs/components/user.vue
@@ -1,18 +1,20 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="people"
-    sort-by="calories"
+    :items="desserts"
+    sort-by="first_name"
     class="elevation-1"
   >
     <template v-slot:top>
       <v-toolbar flat color="white">
-        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-toolbar-title>My App!</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
           <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+            <v-btn color="rgb(231,140,145)" dark class="mb-2" v-on="on"
+              >New Item</v-btn
+            >
           </template>
           <v-card>
             <v-card-title>
@@ -67,7 +69,7 @@
         </v-dialog>
       </v-toolbar>
     </template>
-    <template v-slot:[`item.actions`]="{ item }">
+    <template v-slot:item.action="{ item }">
       <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
       <v-icon small @click="deleteItem(item)">delete</v-icon>
     </template>
@@ -95,7 +97,7 @@ export default {
       { text: "Task", value: "task" },
       { text: "Actions", value: "action", sortable: false },
     ],
-    people: [],
+    desserts: [],
     editedIndex: -1,
     editedItem: {
       first_name: "",
@@ -131,7 +133,7 @@ export default {
         .get("http://localhost:3000/users")
         .then((response) => {
           console.log(response.data);
-          this.people = response.data;
+          this.desserts = response.data;
         })
         .catch((e) => {
           console.log(e);
@@ -143,7 +145,7 @@ export default {
       this.dialog = true;
     },
     deleteItem(item) {
-      const index = this.people.indexOf(item);
+      const index = this.desserts.indexOf(item);
       confirm("Are you sure you want to delete this item?");
       axios
         .delete(`http://localhost:3000/users/${item.id}`)
@@ -156,7 +158,7 @@ export default {
         .catch((error) => {
           console.log(error);
         });
-      this.people.splice(index, 1);
+      this.desserts.splice(index, 1);
     },
     close() {
       this.dialog = false;
@@ -197,7 +199,7 @@ export default {
           .catch((error) => {
             console.log(error);
           });
-        this.people.push(this.editedItem);
+        this.desserts.push(this.editedItem);
       }
       this.close();
     },
@@ -205,7 +207,7 @@ export default {
       axios
         .get(`http://localhost:3000/${item.id}`)
         .then((response) => {
-          this.people = response.data;
+          this.dessert = response.data;
         })
         .catch((error) => {
           console.log(error);
